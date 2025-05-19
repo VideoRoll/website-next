20878440dd0305ef39ecb087939b30f376b70fcd
gomi <gxy880520@qq.com>
2025-05-19 21:37:12 +0800

    chore: update dockerignore

--- packages/documentation/next.config.mjs
+++ packages/documentation/next.config.mjs
@@ -3,7 +3,6 @@ import nextra from "nextra";
 // Set up Nextra with its configuration
 const withNextra = nextra({
   // ... Add Nextra-specific options here
-  contentDirBasePath: "/docs",
 });
 
 // Export the final Next.js config with Nextra included
